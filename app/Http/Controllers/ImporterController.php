4785ad8dbf810fb96fa16f549407bad3ca6d354e
Leoni Mella <leoni.mella@webhub.com.br>
2018-07-12 08:57:40 -0300

    Created ImporterController to handle file submission. Create 'api/importer/csv' route to handle csv file submissions

---
+++ app/Http/Controllers/ImporterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImporterController extends Controller
+{
+    public function handleCSVFile(Request $request)
+    {
+        dd($request);
+    }
+}
