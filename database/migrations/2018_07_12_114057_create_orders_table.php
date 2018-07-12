deb7d00605f2b94526199354964f1221988a060e
Leoni Mella <leoni.mella@webhub.com.br>
2018-07-12 08:49:02 -0300

    Created model Order and migrations to create table 'orders'

---
+++ database/migrations/2018_07_12_114057_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('qty');
+            $table->float('thickness');
+            $table->string('material');
+            $table->float('bending');
+            $table->float('threading');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
